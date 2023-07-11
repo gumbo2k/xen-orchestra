4018de7d44f0ddd2917d63afb599eaa1dc55f68a
mathieuRA <contact@mathieu-raisin.fr>
2023-07-11 17:02:36 +0200

    feat(xo-web/xostor): button install packages

--- packages/xo-web/src/xo-app/xostor/index.js
+++ packages/xo-web/src/xo-app/xostor/index.js
@@ -11,6 +11,8 @@ import { SelectPool } from '../../common/select-objects'
 import { createGetObjectsOfType } from '../../common/selectors'
 import { toggleState } from '../../common/reaclette-utils'
 import { Pool } from '../../common/render-xo-item'
+import ActionButton from '../../common/action-button'
+import Icon from 'icon'
 
 const N_HOSTS_MIN = 3
 const N_HOSTS_MAX = 7
@@ -61,12 +63,16 @@ export default decorate([
         const hostsPool = Object.values(props.hostsByPoolId[pool.id])
         return isGoodNumberOfHosts(hostsPool) && isXcpngPool(hostsPool[0]) && !alreadyHasXostore()
       },
+
       // Selected Pool ===========================================================
       isXcpngPool: state => isXcpngPool(state.hostsPool[0]),
       isPoolGoodNumberOfHosts: state => isGoodNumberOfHosts(state.hostsPool),
       poolAlreadyHasXostore: () => alreadyHasXostore(),
       isPoolCompatibleXostore: state =>
         state.isPoolGoodNumberOfHosts && state.isXcpngPool && !state.poolAlreadyHasXostore,
+
+      // Hosts Pool ==============================================================
+      hostsIsMissingPackages: () => true, // @TODO: Check if at least one host need to install packages
     },
   }),
   injectState,
@@ -74,32 +80,55 @@ export default decorate([
     return (
       <Page header={HEADER}>
         <Container>
-          <label>
-            <input
-              type='checkbox'
-              checked={state.onlyShowXostorePools}
-              onChange={effects.toggleState}
-              name='onlyShowXostorePools'
-            />{' '}
-            Only show pools that meet XOSTOR requirements
-          </label>
-          <SelectPool predicate={state.xostorePoolPredicate} value={state.poolId} onChange={effects.onChange} />
-          {/* ERRORS */}
-          {state.poolId !== undefined && !state.isPoolCompatibleXostore && (
-            <div className='text-danger'>
-              {/* @TODO: add href */}
-              <p className='mb-0'>
-                <Pool id={state.poolId} link /> does not meet the requirements for XOSTOR. Refer to the{' '}
-                <a href='#'>documentation</a>
-              </p>
-              <ul>
-                {!state.isXcpngPool && <li>Not an XCP-ng pool</li>}
-                {!state.isPoolGoodNumberOfHosts && <li>Wrong number of hosts</li>}
-                {state.poolAlreadyHasXostore && <li>Already have a XOSTOR storage</li>}
-              </ul>
-            </div>
-          )}
-          {/* ERRORS */}
+          {/* Pool Selector */}
+          <div className='mb-1'>
+            <label>
+              <input
+                type='checkbox'
+                checked={state.onlyShowXostorePools}
+                onChange={effects.toggleState}
+                name='onlyShowXostorePools'
+              />{' '}
+              Only show pools that meet XOSTOR requirements
+            </label>
+            <SelectPool predicate={state.xostorePoolPredicate} value={state.poolId} onChange={effects.onChange} />
+            {state.poolId !== undefined && !state.isPoolCompatibleXostore && (
+              <div className='text-danger'>
+                {/* @TODO: add href */}
+                <p className='mb-0'>
+                  <Pool id={state.poolId} link /> does not meet the requirements for XOSTOR. Refer to the{' '}
+                  <a href='#'>documentation</a>
+                </p>
+                <ul>
+                  {!state.isXcpngPool && <li>Not an XCP-ng pool</li>}
+                  {!state.isPoolGoodNumberOfHosts && <li>Wrong number of hosts</li>}
+                  {state.poolAlreadyHasXostore && <li>Already have a XOSTOR storage</li>}
+                </ul>
+              </div>
+            )}
+          </div>
+          {/* Install packages */}
+          <div>
+            <em>
+              <Icon icon='info' /> On each hosts, "xcp-ng-release-linstor" and "xcp-ng-linstor" will be installed. You
+              also can install them manually
+            </em>
+            <br />
+            <ActionButton
+              btnStyle='primary'
+              disabled={!state.isPoolCompatibleXostore || !state.hostsIsMissingPackages}
+              tooltip={
+                !state.isPoolCompatibleXostore
+                  ? 'Invalid pool'
+                  : !state.hostsIsMissingPackages
+                  ? 'Hosts already have packages'
+                  : undefined
+              }
+              icon='menu-xosan'
+            >
+              Install packages
+            </ActionButton>
+          </div>
         </Container>
       </Page>
     )
